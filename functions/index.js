1d736fd822c754b14917d43c2455de000f84ade7
jjjasmin <yasumi723@gmail.com>
2026-08-13 15:53:50 +0900

    manual: push all changes to main

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,25 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-  // ?v= の値を取得
   const videoId = url.searchParams.get('v');
 
-  // 本来の HTML レスポンスを取得
   const response = await context.env.ASSETS.fetch(context.request);
 
-  // v パラメータがない場合はそのまま返す
   if (!videoId) {
     return response;
   }
 
-  // YouTubeサムネイル画像のURL（hqdefault: 高画質）
   const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
 
-  // HTMLRewriter で meta タグを動的に書き換え
   return new HTMLRewriter()
-    .on('meta[property="og:image"]', {
+    // og:image を書き換え
+    .on('meta', {
       element(element) {
-        element.setAttribute('content', thumbnailUrl);
-      }
-    })
-    .on('meta[name="twitter:image"]', {
-      element(element) {
-        element.setAttribute('content', thumbnailUrl);
+        const property = element.getAttribute('property');
+        const name = element.getAttribute('name');
+
+        if (property === 'og:image' || name === 'twitter:image') {
+          element.setAttribute('content', thumbnailUrl);
+        }
       }
     })
     .transform(response);
